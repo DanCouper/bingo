5a02ef0d38e2d2784565298b679b2b0b72ad6513
DanCouper <danielcouper81@googlemail.com>
2018-05-17 00:22:49 +0100

    Added explanatory comments

--- Bingo.elm
+++ Bingo.elm
@@ -2,6 +2,11 @@ module Bingo exposing (..)
 
 import Html
 
-
+{- All Elm apps must have a `main` function - this is
+   the entry point to the application. -}
 main =
     Html.text "Hello, World!"
+
+{- This can be ran and recompiled etc using the `elm reactor`
+   CLI program. This is great for quick feedback, but breaks
+   down with apps dependent on external CSS/JS etc. -}
