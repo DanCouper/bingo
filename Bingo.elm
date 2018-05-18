ff1cc92d6726d806539fc35082db905e58b07502
DanCouper <danielcouper81@googlemail.com>
2018-05-18 22:24:07 +0100

    Example of piping functions

--- Bingo.elm
+++ Bingo.elm
@@ -6,7 +6,17 @@ import Html
 {- All Elm apps must have a `main` function - this is
    the entry point to the application.
 -}
+-- main =
+--     Html.text (String.repeat 3 (String.toUpper "Bingo!"))
+-- Instead of the above, can use pipe operator as expected:
+-- main =
+--     "Bingo!"
+--         |> String.toUpper
+--         |> String.repeat 3
+--         |> Html.text
 
 
 main =
-    Html.text "Bingo!"
+    "Bingo"
+        |> String.toUpper
+        |> Html.text
